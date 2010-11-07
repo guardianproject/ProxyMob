5f39157315d4cc01a8c585f481bcdf2bbc7fb4f1
The Guardian Project <root@guardianproject.info>
2010-11-07 01:40:08 -0500

    new readme, and bug fix in overlay

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-//
+*/
 //this whole code exists because we can't do integer settings in the options.xul file. lame.
 
 var proxymob = {
